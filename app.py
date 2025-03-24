6c91737dd155d5115fa90d04460988658422a66d
jtking <jtking@diversitech.com>
2025-03-24 16:52:40 -0400

    changed run parameters. ready for cloud again

--- app.py
+++ app.py
@@ -493,5 +493,5 @@ def time_data_api():
         })
 
 if __name__ == '__main__':
-    #app.run()  # remove debug=true
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run()  # remove debug=true
+    #app.run(host='0.0.0.0', port=5000, debug=True)
